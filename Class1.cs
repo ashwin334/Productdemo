ff13defda84dcb4c6f643e164bec31af805701af
ashwin333 <ashwinpatil832@gmail.com>
2022-10-28 12:00:58 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productdemo
+{
+    internal class Product
+    {
+        private int id;
+        private string name;
+        double price;
+
+        public void add(int id, string name, double price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+        public string Print()
+        {
+            return $" Product:{id}/{name}/{price}";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product();
+            p1.add(93, "Ashwin", 75.7);
+            Console.WriteLine(p1.Print());
+        }
+    }
+}
